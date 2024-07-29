b7c53bc34490e223d5676eb48bd11b5d4af29342
Antti Hätinen <pharazon@phz.fi>
2024-07-29 17:30:28 +0300

    Fixed cuda installation

--- setup.sh
+++ setup.sh
@@ -23,9 +23,9 @@ sudo apt-get install -y linux-headers-$(uname -r)
 sudo apt-key del 7fa2af80
 distro=ubuntu2404
 arch=x86_64
-wget https://developer.download.nvidia.com/compute/cuda/repos/<distro>/<arch>/cuda-archive-keyring.gpg
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-archive-keyring.gpg
 sudo mv cuda-archive-keyring.gpg /usr/share/keyrings/cuda-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/<distro>/<arch>/ /" | sudo tee /etc/apt/sources.list.d/cuda-$distro-$arch.list
+echo "deb [signed-by=/usr/share/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/ /" | sudo tee /etc/apt/sources.list.d/cuda-$distro-$arch.list
 sudo apt update
 sudo apt-get install -y cuda-toolkit nvidia-gds
 
