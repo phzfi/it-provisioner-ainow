8f6fa34a98334d9a7877f8ff1f574e165e094967
Antti Hätinen <pharazon@phz.fi>
2024-07-29 19:14:30 +0300

    Fix cuda key management and docker group add

--- setup.sh
+++ setup.sh
@@ -23,9 +23,12 @@ sudo apt-get install -y linux-headers-$(uname -r)
 sudo apt-key del 7fa2af80
 distro=ubuntu2404
 arch=x86_64
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-archive-keyring.gpg
-sudo mv cuda-archive-keyring.gpg /usr/share/keyrings/cuda-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/ /" | sudo tee /etc/apt/sources.list.d/cuda-$distro-$arch.list
+#wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-archive-keyring.gpg
+#sudo mv cuda-archive-keyring.gpg /etc/apt/keyrings/cuda-archive-keyring.gpg
+#echo "deb [signed-by=/etc/apt/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/ /" | sudo tee /etc/apt/sources.list.d/cuda-$distro-$arch.list
+#TODO use legacy key management
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/3bf863cc.pub
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/ /" | sudo tee /etc/apt/sources.list.d/cuda-$distro-$arch.list
 sudo apt update
 sudo apt-get install -y cuda-toolkit nvidia-gds
 
@@ -43,7 +46,7 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-gpasswd -a ainow docker
+sudo gpasswd -a ainow docker
 
 
 echo "Rebooting next, ctrl-c to cancel, enter to reboot"
