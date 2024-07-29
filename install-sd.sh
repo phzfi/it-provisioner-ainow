8b7377427bc85fda34f5b0d5de529cb41a59c092
Antti Hätinen <pharazon@phz.fi>
2024-07-29 19:33:26 +0300

    Install SD

---
+++ install-sd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Install Stable Diffusion Docker
+
+mkdir -p ~/workspace/docker
+cd ~/workspace/docker
+git clone https://github.com/AbdBarho/stable-diffusion-webui-docker.git
+cd stable-diffusion-webui-docker
+
+docker compose --profile download up --build
+docker compose --profile auto up --build
